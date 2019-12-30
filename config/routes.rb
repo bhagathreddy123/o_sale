798e770f78dcb78f7ad427306f6997a906197be7
BhagathReddy-K <bkatta@accela.com>
2019-12-30 18:19:26 +0530

    creating and updating product & dry the new,edit forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show, :new,:edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
+  get "/products/:id/edit", to: "products#edit"
+  put "/products/:id", to: "products#update"
+  get "/products/:id", to: "products#show"
 end
